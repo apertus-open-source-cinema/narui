8b3fb8a10915327befe0a7ea163f5289288fdef4
Jaro Habiger <jarohabiger@googlemail.com>
2021-04-27 01:05:19 +0200

    big performance improvement by using jemalloc

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod heart;
 pub mod theme;
 pub mod vulkano_render;
 pub mod widgets;
+
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
