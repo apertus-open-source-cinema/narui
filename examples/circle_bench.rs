15dc1978846888b988651b8e0cc5d92c6e4c6262
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-08-25 15:48:21 +0200

    fix circle bench

--- examples/circle_bench.rs
+++ examples/circle_bench.rs
@@ -1,5 +1,40 @@
-use narui::{style::*, *};
+use narui::*;
 use winit::{platform::unix::WindowBuilderExtUnix, window::WindowBuilder};
+/*
+                            <fill_rect
+                                key=&y
+                                fill_color={
+                                    let val = context.listen(frame_counter);
+                                    Some(Color::from_components((x as f32 / 50., y as f32 / 50., ((val as f32 / 10.0).sin() + 1.) / 2., 1.)))
+                                }
+                            />
+*/
+#[widget]
+pub fn top(context: &mut WidgetContext) -> Fragment {
+    let frame_counter = context.listenable(0);
+    context.after_frame(move |context| {
+        context.shout(frame_counter, context.spy(frame_counter) + 1);
+    });
+
+    rsx! {
+        <row main_axis_alignment=MainAxisAlignment::SpaceEvenly>
+            {(0..50).map(|x| rsx!{
+                <column main_axis_alignment=MainAxisAlignment::SpaceEvenly key=&x>
+                    {(0..50).map(|y| rsx! {
+                        <sized_box constraint=BoxConstraints::tight_for(rutter_layout::Size::new(10.0, 10.0)) key=&y>
+                            <fill_rect
+                                color={
+                                    let val = context.listen(frame_counter);
+                                    Color::from_components((x as f32 / 50., y as f32 / 50., ((val as f32 / 10.0).sin() + 1.) / 2., 1.))
+                                }
+                            />
+                        </sized_box>
+                    }).collect()}
+                </column>
+            }).collect()}
+        </row>
+    }
+}
 
 #[widget]
 pub fn top(context: Context) -> Fragment {
