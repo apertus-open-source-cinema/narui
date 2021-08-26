ebe72e712fea796be66e1414ea23b8d4e06cde1c
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-08-26 02:30:08 +0200

    compress keys even further

--- narui_macros/src/rsx_macro.rs
+++ narui_macros/src/rsx_macro.rs
@@ -8,22 +8,6 @@ use syn_rsx::{Node, NodeType};
 
 pub fn rsx(input: proc_macro::TokenStream) -> TokenStream {
     let mut parsed = syn_rsx::parse2(input.into()).unwrap();
-    if parsed.len() == 0 {
-        let loc = {
-            let mut s = DefaultHasher::new();
-            (format!("{:?}", Span::call_site())).hash(&mut s);
-            s.finish()
-        };
-        return quote! {{
-            Fragment {
-                key: context.widget_local.key.with(KeyPart::Rsx(#loc)),
-                gen: std::rc::Rc::new(move |_context| FragmentInner::Node {
-                    layout: Box::new(rutter_layout::Transparent),
-                    children: vec![],
-                }),
-            }
-        }};
-    }
     assert_eq!(parsed.len(), 1, "the rsx macro can have at maximum one child");
     let (beginning, inplace) = handle_rsx_node(parsed.remove(0));
 
@@ -40,20 +24,27 @@ fn handle_rsx_node(x: Node) -> (TokenStream, TokenStream) {
         let name = x.name.as_ref().unwrap();
         let node_name = name;
         let name_str = name.to_string();
-        let loc = {
-            let LineColumn { line, column } = name.span().start();
-            // only every fourth part of the column is relevant, because the minimum size
-            // for a rsx node is four: <a />
-            let column = (column / 4) & 0b11_1111;
-            // use 6 bits for column (good for up to 256 columns)
-            // use 10 bits for line (good for up to 1024 lines)
-            let line = (line & 0b11_1111_1111) << 6;
-            (column as u16) | (line as u16)
-        };
+        let LineColumn { line, column } = name.span().start();
 
         let args_listenable_ident =
-            Ident::new(&format!("__{}_{}_args", name_str, loc), Span::call_site());
-        let mut key = quote! {KeyPart::Fragment { widget_id: #name::WIDGET_ID.load(std::sync::atomic::Ordering::SeqCst), location_id: #loc }};
+            Ident::new(&format!("__{}_{}_{}_args", name_str, line, column), Span::call_site());
+        let loc = quote! {
+            {
+                let (widget_line, widget_column) = context.widget_loc;
+                let line = #line - widget_line;
+                let column = #column - widget_column;
+                // only every fourth part of the column is relevant, because the minimum size
+                // for a rsx node is four: <a />
+                let column = (column / 4) & 0b11_1111;
+                // use 6 bits for column (good for up to 256 columns)
+                // use 10 bits for line (good for up to 1024 lines)
+                let line = (line & 0b11_1111_1111) << 6;
+                (column as u16) | (line as u16)
+            }
+        };
+        let mut key = quote! {
+            KeyPart::Fragment { widget_id: #node_name::WIDGET_ID.load(std::sync::atomic::Ordering::SeqCst), location_id: #loc }
+        };
 
         let constructor_path = {
             let constructor_ident =
@@ -66,7 +57,7 @@ fn handle_rsx_node(x: Node) -> (TokenStream, TokenStream) {
             let name = attribute.name.as_ref().unwrap();
             let value = attribute.value.as_ref().unwrap().clone();
             if name.to_string() == "key" {
-                key = quote! {KeyPart::FragmentKey { widget_id: #node_name::WIDGET_ID.load(std::sync::atomic::Ordering::SeqCst), location_id: #loc, key: #value }}
+                key = quote! { KeyPart::FragmentKey { widget_id: #node_name::WIDGET_ID.load(std::sync::atomic::Ordering::SeqCst), location_id: #loc, key: #value }}
             } else {
                 processed_attributes.push(quote! {#name=#value});
             }
