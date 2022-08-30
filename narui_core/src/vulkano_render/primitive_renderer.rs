apertus-open-source-cinema/narui

commit e9147edccd11b53baae8bbe5173d04f21f4c545a

fixup: use TriangleList again
previously we wrongly used TriangleStrip

narui_core/src/vulkano_render/primitive_renderer.rs
@@ -303,7 +303,7 @@ impl Renderer {
             .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
             .vertex_shader(vs.entry_point("main").unwrap(), ())
             .input_assembly_state(
-                InputAssemblyState::new().topology(PrimitiveTopology::TriangleStrip),
+                InputAssemblyState::new().topology(PrimitiveTopology::TriangleList),
             )
             .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
             .fragment_shader(fs.entry_point("main").unwrap(), ())

narui_widgets/src/subpass.rs
@@ -143,7 +143,7 @@ pub fn raw_blur(
             let pipeline = GraphicsPipeline::start()
                 .vertex_shader(vs.entry_point("main").unwrap(), ())
                 .input_assembly_state(
-                    InputAssemblyState::new().topology(PrimitiveTopology::TriangleStrip),
+                    InputAssemblyState::new().topology(PrimitiveTopology::TriangleList),
                 )
                 .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
                 .fragment_shader(fs.entry_point("main").unwrap(), ())

commit f2713040ff0d7a31842a2af8be12515ccb9d28be

fix subpass rendering
Fixes: e9147edccd11b53baae8bbe5173d04f21f4c545a
Fixes: a4ec9f992b405a6b832045f581e06d9b41f80c71

narui_core/src/vulkano_render/primitive_renderer.rs
@@ -328,6 +328,7 @@ impl Renderer {
             .depth_stencil_state(DepthStencilState {
                 depth: Some(DepthState {
                     compare_op: StateMode::Fixed(CompareOp::LessOrEqual),
+                    write_enable: StateMode::Fixed(true),
                     ..Default::default()
                 }),
                 ..DepthStencilState::simple_depth_test()

narui_widgets/src/subpass.rs
@@ -150,7 +150,7 @@ pub fn raw_blur(
             let pipeline = GraphicsPipeline::start()
                 .vertex_shader(vs.entry_point("main").unwrap(), ())
                 .input_assembly_state(
-                    InputAssemblyState::new().topology(PrimitiveTopology::TriangleList),
+                    InputAssemblyState::new().topology(PrimitiveTopology::TriangleStrip),
                 )
                 .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
                 .fragment_shader(fs.entry_point("main").unwrap(), ())
@@ -173,6 +173,7 @@ pub fn raw_blur(
                 .depth_stencil_state(DepthStencilState {
                     depth: Some(DepthState {
                         compare_op: StateMode::Fixed(CompareOp::LessOrEqual),
+                        write_enable: StateMode::Fixed(true),
                         ..Default::default()
                     }),
                     ..DepthStencilState::simple_depth_test()